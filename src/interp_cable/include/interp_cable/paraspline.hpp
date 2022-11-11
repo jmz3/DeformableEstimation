bf6c0f073d9b7bf2d160295aed3d86c37dc725b1
jeremyzz830 <zjm731215@outlook.com>
2022-11-11 16:26:27 -0500

    merge

---
+++ src/interp_cable/include/interp_cable/paraspline.hpp
@@ -0,0 +1,131 @@
+#include <cstdio>
+#include <cassert>
+#include <vector>
+#include <cmath>
+#include <ros/ros.h>
+
+// f(x) = a_i + b_i*(x-x_i) + c_i*(x-x_i)^2 + d_i*(x-x_i)^3
+// where a_i = y_i, or else it won't go through grid points
+
+namespace Cable
+{
+    class paraspline
+    {
+    public:
+        // interpolation types
+        enum spline_type
+        {
+            linear = 10, // Linear Interpolation
+            cubic = 30,  // Cubic Spline C2 continuous
+        };
+
+        // Boundary Condition type
+        enum bound_type
+        {
+            first_order = 1,  // Given 1st order boundary as initial condition
+            second_order = 2, // Given 2nd order boundary as initial condition
+            not_a_knot = 3    //
+        };
+
+    protected:
+        std::vector<double> m_x, m_y; // input x, y data
+        // interpolation parameters
+
+        std::vector<double> m_b, m_c, m_d; // spline coefficients
+        double m_c0;
+
+        spline_type m_type;
+        bound_type m_left, m_right;
+        double m_left_value, m_right_value;
+        void set_coeffs_from_b();
+        size_t find_closest(double x) const;
+
+    public:
+        paraspline();
+        paraspline(const std::vector<double> &X,
+                   const std::vector<double> &Y,
+                   spline_type type,
+                   bool make_monotonic,
+                   bound_type left,
+                   double left_value,
+                   bound_type right,
+                   double right_value);
+
+        // modify boundary conditions: if called it must be before set_points()
+        void set_boundary(bound_type left, double left_value,
+                          bound_type right, double right_value);
+
+        // set all data points (cubic_spline=false means linear interpolation)
+        void set_points(const std::vector<double> &x,
+                        const std::vector<double> &y,
+                        spline_type type);
+
+        // adjust coefficients so that the spline becomes piecewise monotonic
+        // where possible
+        //   this is done by adjusting slopes at grid points by a non-negative
+        //   factor and this will break C^2
+        //   this can also break boundary conditions if adjustments need to
+        //   be made at the boundary points
+        // returns false if no adjustments have been made, true otherwise
+
+        // evaluates the spline at point x
+        double operator()(double x) const;
+        double deriv(int order, double x) const;
+
+        // solves for all x so that: spline(x) = y
+        std::vector<double> solve(double y, bool ignore_extrapolation = true) const;
+
+        // returns the input data points
+        std::vector<double> get_x() const { return m_x; }
+        std::vector<double> get_y() const { return m_y; }
+        double get_x_min() const
+        {
+            ROS_ASSERT(!m_x.empty());
+            return m_x.front();
+        }
+        double get_x_max() const
+        {
+            ROS_ASSERT(!m_x.empty());
+            return m_x.back();
+        }
+    };
+
+    class band_matrix
+    {
+    private:
+        std::vector<std::vector<double>> m_upper;
+        std::vector<std::vector<double>> m_lower;
+
+    public:
+        band_matrix(){};
+        band_matrix(int dim, int n_u, int n_l);
+        ~band_matrix(){};
+        void resize(int dim, int n_u, int n_l);
+        int dim() const;
+        int num_upper() const
+        {
+            return (int)m_upper.size() - 1;
+        }
+        int num_lower() const
+        {
+            return (int)m_lower.size() - 1;
+        }
+        // access operator
+        double &operator()(int i, int j);      // write
+        double operator()(int i, int j) const; // read
+        double &saved_diag(int i);
+        double saved_diag(int i) const;
+        void lu_decompose();
+        std::vector<double> r_solve(const std::vector<double> &b) const;
+        std::vector<double> l_solve(const std::vector<double> &b) const;
+        std::vector<double> lu_solve(const std::vector<double> &b,
+                                     bool is_lu_decomposed = false);
+    };
+    double get_eps();
+
+    std::vector<double> solve_linear(double a, double b);
+    std::vector<double> solve_quadratic(double a, double b, double c,
+                                        int newton_iter);
+    std::vector<double> solve_cubic(double a, double b, double c, double d,
+                                    int newton_iter);
+};
---
+++ src/interp_cable/src/sort.cpp
@@ -0,0 +1,187 @@
+#include <ros/ros.h>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <sorting/sort.hpp>
+
+Cable::Sort::Sort(double theta_max, double theta_min, double dL, double sigma,
+                  std::vector<double> StartP, std::vector<double> StartN) : theta_max(theta_max), theta_min(theta_min), dL(dL), sigma(sigma), StartP(StartP), StartN(StartN){};
+
+Cable::Sort::~Sort(){};
+
+double Cable::Sort::fdot(std::vector<double> &a, std::vector<double> &b)
+{
+    double dot = 0.0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        dot += a[i] * b[i];
+    };
+    return dot;
+};
+
+std::vector<double> Cable::Sort::fcross(std::vector<double> &a, std::vector<double> &b)
+{
+    std::vector<double> cross;
+    cross.clear();
+    cross.assign(3, 0.0);
+    // std::cout << a.size() << ", " << b.size() << std::endl;
+
+    ROS_ASSERT(a.size() == 3);
+    ROS_ASSERT(b.size() == 3); // Ensure inner product is performed for 3x1 vector.
+
+    // std::cout << "norm of a is: " << fnorm(a) << std::endl;
+    // ROS_INFO("A is ");
+    //     for (auto x : a)
+    // {
+    //     std::cout << x << std::endl;
+    // }
+
+    cross[0] = a[1] * b[2] - a[2] * b[1];
+    cross[1] = a[2] * b[0] - a[0] * b[2];
+    cross[2] = a[0] * b[1] - a[1] * b[0];
+
+    // for (auto x : cross)
+    // {
+    //     std::cout << x << std::endl;
+    // }
+    return cross;
+};
+
+double Cable::Sort::fnorm(std::vector<double> &a)
+{
+    double sum = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        sum += a[i] * a[i];
+    }
+    double norm_result = std::sqrt(sum);
+
+    return norm_result;
+}
+
+double Cable::Sort::fangle(std::vector<double> &a, std::vector<double> &b)
+{
+    std::vector<double> A = fcross(a, b);
+    double B = fnorm(A);
+    double C = fdot(a, b);
+    return atan2(B, C);
+};
+
+void Cable::Sort::fscale(std::vector<double> &a, double scalar)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i] = scalar * a[i];
+    }
+}
+
+std::vector<double> Cable::Sort::frodriguez(std::vector<double> &origin, std::vector<double> &axis, double angle)
+{
+    // a is the original vector
+    // b is an unit vector that describes the rotation axis
+    // angle is the rotation angle along b
+    ROS_ASSERT(fnorm(axis) < 1 + 1e-6);
+    std::vector<double> temp, rot;
+    std::vector<std::vector<double>> comp;
+
+    temp.clear();
+    // component 1 : v*cos(angle)
+    fscale(origin, cos(angle));
+    comp.push_back(origin);
+
+    // component 2 : (k x v)*sin(angle)
+    temp = fcross(axis, origin);
+    fscale(temp, sin(angle));
+    comp.push_back(temp);
+
+    // component 3 : k (k dot v)( 1 - cos(angle) )
+    fscale(axis, fdot(axis, origin) * (1 - cos(angle)));
+    comp.push_back(axis);
+
+    rot.assign(comp[0].size(), 0.0);
+    // ROS_INFO("Good so far 10");
+    for (int i = 0; i < comp[0].size(); i++)
+    {
+        rot[i] = comp[0][i] + comp[1][i] + comp[2][i];
+    }
+
+    return rot;
+}
+
+void Cable::Sort::init(std::vector<std::vector<double>> &ps)
+{
+    Direction.clear();
+    Direction.push_back(StartN);
+
+    ps.insert(ps.begin(), StartP);
+};
+
+void Cable::Sort::fsort(std::vector<std::vector<double>> &ps)
+{
+
+    init(ps);
+    // i < size-1 because
+    // there's no need to find the next point for the last one
+    for (int i = 0; i < ps.size() - 1; i++)
+    {
+        Prob.clear();
+        Prob.assign(ps.size(), 0.0);
+
+        for (int j = i + 1; j < ps.size(); j++)
+        {
+            PositionalDiff.clear();
+
+            for (int k = 0; k < 3; k++)
+            {
+                // PositionalDiff = (pi+1) - (pi)
+                PositionalDiff.push_back(ps[j][k] - ps[i][k]);
+            }
+            // for (auto x : PositionalDiff)
+            // {
+            //     std::cout << x << ", ";
+            // }
+            // std::cout << std::endl;
+
+            // find the angle theta
+            theta = fangle(Direction[i], PositionalDiff);
+
+            if (theta < theta_max && theta >= theta_min)
+            {
+                mean = 2 * dL * cos(theta) / (M_PI - 2 * theta);
+                Prob[j] = (1 / (sigma * std::sqrt(2 * M_PI))) * exp(-0.5 * pow(((fnorm(PositionalDiff) - mean) / sigma), 2));
+            }
+        }
+
+        // Find the max probability and its corresponding index
+        int MaxIdx = std::max_element(Prob.begin(), Prob.end()) - Prob.begin();
+        // for (auto x : Prob)
+        // {
+        //     std::cout << x << ", ";
+        // }
+        // std::cout << std::endl;
+        // std::cout << MaxIdx << std::endl;
+        swap(ps[i + 1], ps[MaxIdx]);
+
+        PositionalDiff.clear();
+        for (int k = 0; k < 3; k++)
+        {
+            // PositionalDiff = (pi+1) - (pi)
+            PositionalDiff.push_back(ps[i + 1][k] - ps[i][k]);
+        }
+
+        Normal = fcross(Direction[i], PositionalDiff);
+
+        // Normalize ( Scale to 0 - 1 )
+        fscale(Normal, 1.0 / fnorm(Normal));
+        ROS_INFO_STREAM("Norm of Normal is"<<fnorm(Normal));
+        if(fnorm(Normal) - 1 < 1e-6) break; // Ensure the normal is unit vector
+
+        rot_angle = fangle(Direction[i], PositionalDiff);
+
+        std::vector<double> direction_rot = frodriguez(Direction[i], Normal, 2 * rot_angle);
+
+        Direction.push_back(direction_rot);
+    }
+    ROS_INFO("Sort Complete!");
+};
